d3dfcb4c05f7ff33753ecab9c3a8fc6191868552
Mrinmay12 <88670421+Mrinmay12@users.noreply.github.com>
2024-05-12 20:00:46 +0530

    Delete Pages directory

--- Pages/Location.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import UserCard from "../Component/UserInfoCard/UserCard"
-export default function Location() {
-  return (
-    <div>
-      <UserCard/>
-    </div>
-  )
-}
