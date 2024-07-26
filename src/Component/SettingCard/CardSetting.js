e6821a51dd2dadc0b7823daeca52a4e05f36f110
Mrinmay12 <88670421+Mrinmay12@users.noreply.github.com>
2024-07-26 18:06:37 +0530

    Add files via upload

---
+++ src/Component/SettingCard/CardSetting.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./CardSetting.css"
+export default function CardSetting() {
+  return (
+    <div>
+        <div class="settingcard">
+        <div class="settingdescription">
+            <h3>Card Title</h3>
+        
+        </div>
+        <div class="arrow">
+            &rarr;
+        </div>
+    </div>
+    </div>
+  )
+}
