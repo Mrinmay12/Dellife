8c48197a80fd580e58a8ed5576a244f59c7dd1ce
Mrinmay12 <88670421+Mrinmay12@users.noreply.github.com>
2024-08-28 16:55:48 +0530

    Update Firebase.js

--- src/FirebaseConfig/Firebase.js
+++ src/FirebaseConfig/Firebase.js
@@ -9,12 +9,12 @@ const firebaseConfig = {
     // appId: "1:430783478396:web:8885eee6c1850743a667f9",
     // measurementId: "G-QLG650LM87"
     apiKey: "AIzaSyD1omNZoFqNY7zNpaDDSc7E0wAKK1GPKDQ",
-    authDomain: "node-45d47.firebaseapp.com",
-    projectId: "node-45d47",
-    storageBucket: "node-45d47.appspot.com",
-    messagingSenderId: "789729373593",
-    appId: "1:789729373593:web:e45b73299436f770aa3708",
-    measurementId: "G-T12BWLCX06"
+  authDomain: "node-45d47.firebaseapp.com",
+  projectId: "node-45d47",
+  storageBucket: "node-45d47.appspot.com",
+  messagingSenderId: "789729373593",
+  appId: "1:789729373593:web:e45b73299436f770aa3708",
+  measurementId: "G-T12BWLCX06"
   };
   const app = initializeApp(firebaseConfig);
   const storage = getStorage(app);
